e566effa26741b9617da09df4244754bd6a38789
abhe6819 <abhe6819@colorado.edu>
2020-04-09 21:41:28 -0600

    added to problem 5

--- Labs/Lab11/Hein_lab11.R
+++ Labs/Lab11/Hein_lab11.R
@@ -3,12 +3,27 @@ WoodDensity <- read.csv (file = "GlobalWoodDensityDatabase-Data.csv", stringsAsF
 names(WoodDensity)[4] <- "Density" #Renamed col 4 which did not translate well into R
 
 # lab step 4a: find NA value in WoodDensity
-unlist (lapply (WoodDensity, function (x){
-  which (is.na (x))
-  }))
+which(is.na(WoodDensity$Density))
 # lab step 4b: remove NA row
+library("tidyr") #access tidyr library
 DensityNoNA <- drop_na(WoodDensity)
 
 # lab step 5: compress data so that each species is only listed once
-library("dplyr")
+library("dplyr") #access dplyr library
 DensityCompressed <- summarise(group_by(DensityNoNA,Binomial,Family), MeanDensity = mean(Density))
+#using base R
+TreeSpecies <- sort(unique (DensityNoNA$Binomial)) #list of species
+NumSpecies <- length(TreeSpecies) # number of unique species
+# pre-allocation
+DensityCompressed2 <- data.frame (Binomial = TreeSpecies,
+                                  Family = rep("", NumSpecies),
+                                  Density = rep(0, NumSpecies),
+                                  stringsAsFactors = F)
+for (ii in 1:nrow(DensityNoNA)) {
+  Species <- TreeSpecies[ii]
+  rows <- which(DensityNoNA$BBinomial == Species)
+  DensityCompressed2$Density[ii] <- mean(DensityNoNA$Density[TreeSpecies[ii]])
+  DensityCompressed2$Family[ii] <- DensityNoNA$Family[TreeSpecies[ii]]
+} 
+
+# lab step 6:
